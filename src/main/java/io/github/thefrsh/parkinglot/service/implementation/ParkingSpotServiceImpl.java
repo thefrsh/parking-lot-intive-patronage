92fbd4986ed7baec792d23959a416b2db5c4eb48
Michal Broniewicz <mbroniewicz@outlook.com>
2021-11-21 15:41:30 +0100

    ParkingSpot Controller and Service stub

--- src/main/java/io/github/thefrsh/parkinglot/service/implementation/ParkingSpotServiceImpl.java
+++ src/main/java/io/github/thefrsh/parkinglot/service/implementation/ParkingSpotServiceImpl.java
@@ -1,9 +1,16 @@
 package io.github.thefrsh.parkinglot.service.implementation;
 
+import io.github.thefrsh.parkinglot.dto.response.ParkingSpotResponse;
 import io.github.thefrsh.parkinglot.service.ParkingSpotService;
+import io.vavr.collection.List;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ParkingSpotServiceImpl implements ParkingSpotService {
 
+    @Override
+    public List<ParkingSpotResponse> getParkingSpots(Boolean available) {
+
+        return List.empty();
+    }
 }
