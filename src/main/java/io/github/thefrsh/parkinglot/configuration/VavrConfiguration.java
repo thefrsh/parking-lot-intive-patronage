fe81afb868724a8edd74205a348edd1e218aa296
Michal Broniewicz <mbroniewicz@outlook.com>
2021-11-21 15:17:22 +0100

    ModelMapper and Vavr configurations

---
+++ src/main/java/io/github/thefrsh/parkinglot/configuration/VavrConfiguration.java
@@ -0,0 +1,16 @@
+package io.github.thefrsh.parkinglot.configuration;
+
+import com.fasterxml.jackson.databind.Module;
+import io.vavr.jackson.datatype.VavrModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class VavrConfiguration {
+
+    @Bean
+    public Module vavrJacksonModule() {
+
+        return new VavrModule();
+    }
+}
