0c83aaf7430b3b143718c47f5e748577d4fe0aa9
Michal Broniewicz <mbroniewicz@outlook.com>
2022-03-23 21:14:59 +0100

    Remove repeated VcsRoot from ProductionProject

--- .teamcity/project/production/ParkingLotProductionProject.kt
+++ .teamcity/project/production/ParkingLotProductionProject.kt
@@ -2,13 +2,10 @@ package project.production
 
 import jetbrains.buildServer.configs.kotlin.v2019_2.Project
 import project.production.build.docker.ParkingLotProductionDockerBuild
-import project.production.vcs.ParkingLotProductionVcsRoot
 
 object ParkingLotProductionProject : Project({
 
     name = "Production"
-
-    vcsRoot(ParkingLotProductionVcsRoot)
 
     buildType(ParkingLotProductionDockerBuild)
 })
