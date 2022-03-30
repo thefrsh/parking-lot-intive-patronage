bb184191549dc574877446547d49cb4725d3f9e7
Michal Broniewicz <mbroniewicz@outlook.com>
2022-03-30 21:25:59 +0200

    Drop version to 2021.1

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -2,7 +2,7 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.*
 import project.production.ParkingLotProductionProject
 import project.production.vcs.ParkingLotProductionVcsRoot
 
-version = "2021.2"
+version = "2021.1"
 
 project {
 
